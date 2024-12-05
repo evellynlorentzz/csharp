ff0f36b1a6387b7ba952352702e8261edd0c0d7f
evellynlorentzz <157728691+evellynlorentzz@users.noreply.github.com>
2024-12-05 13:52:06 -0300

    um monte
    um monte

---
+++ NetBeansProjects/soma/src/main/java/com/mycompany/soma/Color.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.soma;
+
+/**
+ *
+ * @author 24153578
+ */
+public class Color {
+    
+}
---
+++ NetBeansProjects/soma/src/main/java/com/mycompany/soma/Soma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soma;
+
+/**
+ *
+ * @author 24153578
+ */
+public class Soma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
